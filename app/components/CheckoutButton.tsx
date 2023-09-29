feae877062df0c5d04a9a4819b26e315a5f138fd
Mykola Havrylov <n.havrylov@skeletoncrew.dev>
2023-09-29 07:26:27 +0200

    init

---
+++ app/components/CheckoutButton.tsx
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+
+function CheckoutButton() {
+	const handleSubmit = async () => {
+		const hostUrl = `${window.location}success`.toString();
+		console.log('hostUrl', hostUrl);
+
+		const requestData = {
+			order_id: uuidv4(),
+			order_desc: 'test order',
+			currency: 'USD',
+			amount: '1000',
+			response_url: hostUrl,
+			server_callback_url: '',
+		};
+		await fetch('/api/checkout', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(requestData),
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log('data', data.checkout_url);
+				window.location.href = data.checkout_url;
+			})
+			.catch(error => console.error(error));
+	};
+
+	return (
+		<button
+			type='button'
+			onClick={handleSubmit}
+			className='flex-none flex items-center justify-center h-9 pl-4 pr-4 rounded-md text-slate-300 border border-slate-200'
+		>
+			Checkout
+		</button>
+	);
+}
+
+export default CheckoutButton;
