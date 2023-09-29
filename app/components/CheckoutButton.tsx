8ec7bd0befd577ad0bd5770b4e63005fa5112431
Mykola Havrylov <n.havrylov@skeletoncrew.dev>
2023-09-29 14:21:03 +0200

    add api

--- app/components/CheckoutButton.tsx
+++ app/components/CheckoutButton.tsx
@@ -1,6 +1,49 @@
 import { v4 as uuidv4 } from 'uuid';
 
 function CheckoutButton() {
+	// const handleSubmit = async () => {
+	// 	const hostUrl = `${window.location}success`.toString();
+	// 	console.log('hostUrl', hostUrl);
+
+	// 	const requestData = {
+	// 		order_id: uuidv4(),
+	// 		order_desc: 'test order',
+	// 		currency: 'USD',
+	// 		amount: '1000',
+	// 		response_url: hostUrl,
+	// 		server_callback_url: '',
+	// 	};
+	// 	// await fetch('/api/test', {
+	// 	// 	method: 'POST',
+	// 	// 	headers: {
+	// 	// 		'Content-Type': 'application/json',
+	// 	// 	},
+	// 	// 	body: JSON.stringify(requestData),
+	// 	// })
+	// 	// 	.then(response => response.json())
+	// 	// 	.then(data => {
+	// 	// 		console.log('data', data.checkout_url);
+	// 	// 		window.location.href = data.checkout_url;
+	// 	// 	})
+	// 	// 	.catch(error => console.error(error));
+
+	// 	const response = await fetch('/api/checkout', {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+	// 		},
+	// 		body: JSON.stringify(requestData),
+	// 	});
+
+	// 	try {
+	// 		const data = await response.json();
+	// 		console.log('data', data.data);
+	// 		window.location.href = data.data.checkout_url;
+	// 	} catch (error) {
+	// 		console.error('Error parsing JSON:', error);
+	// 	}
+	// };
+
 	const handleSubmit = async () => {
 		const hostUrl = `${window.location}success`.toString();
 		console.log('hostUrl', hostUrl);
@@ -13,35 +56,19 @@ function CheckoutButton() {
 			response_url: hostUrl,
 			server_callback_url: '',
 		};
-		// await fetch('/api/test', {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/json',
-		// 	},
-		// 	body: JSON.stringify(requestData),
-		// })
-		// 	.then(response => response.json())
-		// 	.then(data => {
-		// 		console.log('data', data.checkout_url);
-		// 		window.location.href = data.checkout_url;
-		// 	})
-		// 	.catch(error => console.error(error));
-
-		const response = await fetch('/api/checkout', {
+		await fetch('/api/test-api', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(requestData),
-		});
-
-		try {
-			const data = await response.json();
-			console.log('data', data.data);
-			window.location.href = data.data.checkout_url;
-		} catch (error) {
-			console.error('Error parsing JSON:', error);
-		}
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log('data', data.checkout_url);
+				window.location.href = data.checkout_url;
+			})
+			.catch(error => console.error(error));
 	};
 
 	return (
---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyAjcsb81jRxmWNWh7czoRnHfu_AHYX3b3A',
+	authDomain: 'fondy-test.firebaseapp.com',
+	projectId: 'fondy-test',
+	storageBucket: 'fondy-test.appspot.com',
+	messagingSenderId: '980360172723',
+	appId: '1:980360172723:web:c18d7aa8b26f78e1014cc6',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
